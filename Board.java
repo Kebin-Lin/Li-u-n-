7f46e0ef5825c0950c51eb80e506453d0550fc7f
Kevin Lin <33496321+Stuy-Kevin-Lin@users.noreply.github.com>
2018-01-08 19:11:18 -0500

    Updated addBlock()

--- Board.java
+++ Board.java
@@ -1,52 +1,55 @@
 import java.util.ArrayList;
 
 public class Board {
-    public int num0s;
-    public Block[][] _board;
-    
-    public Board() {
-	num0s = 16;
-	_board = new Block[2][2];
-	for (int i = 0; i < 2; i++) {
-	    for (int i2 = 0; i2 < 2; i++) {
-		_board[i][i2] = new Block();
-	    }
-	}
+  public int _maxNum;
+  public int _num0s;
+  public Block[][] _board;
+
+  public Board() {
+    _maxNum = 0;
+    _num0s = 16;
+    _board = new Block[2][2];
+    for (int i = 0; i < 2; i++) {
+      for (int i2 = 0; i2 < 2; i++) {
+        _board[i][i2] = new Block();
+      }
     }
-    /*
-    public void addBlock() {//Adds a block of value 2 or 4 on an empty space
-	ArrayList<int[]> empty = new ArrayList<int[]>(); //ArrayList for empty spots
-	
-	for (int i = 0; i < 2; i++) {//Look through the _board for empty spots
-	    for (int i2 = 0; i < 2; i++) {
-		if (_board[i][i2].isEmpty()) {//Add empty spot into the ArrayList
-		    int[] adding = new int[2];
-		    adding[0] = i;
-		    adding[1] = i2;
-		    empty.add(adding);
-		}
-	    }
-	}
+  }
+  /*
+  public void addBlock() {//Adds a block of value 2 or 4 on an empty space
+    ArrayList<int[]> empty = new ArrayList<int[]>(); //ArrayList for empty spots
 
-	if (empty.size() == 0) {return;}//D
-
-	int[] selected = empty.get((int)(empty.size() * Math.random()));
-
-	_board[ selected[0] ][ selected[1] ].set( ((int)(Math.random()*2) + 1 ) * 2);
-    } */
-
-    public void addBlock() {
-	int pos = (int)(Math.random() * num0s);
-	int index = 0;
-	for (int r = 0; r < 4; r++) {
-	    for (int c = 0; c < 4; c++) {
-		if (index == pos) {
-		    board[r][c] = ((int)(Math.random()*2)+1)*2;
-		    num0s--;
-		    return;
-		}
-		index++;
-	    }
-	}
+    for (int i = 0; i < 2; i++) {//Look through the _board for empty spots
+      for (int i2 = 0; i < 2; i++) {
+        if (_board[i][i2].isEmpty()) {//Add empty spot into the ArrayList
+          int[] adding = new int[2];
+          adding[0] = i;
+          adding[1] = i2;
+          empty.add(adding);
+        }
+      }
     }
+
+    if (empty.size() == 0) {return;}//D
+
+    int[] selected = empty.get((int)(empty.size() * Math.random()));
+
+    _board[ selected[0] ][ selected[1] ].set( ((int)(Math.random()*2) + 1 ) * 2);
+  } */
+
+  public void addBlock() {
+    int pos = (int)(Math.random() * _num0s);
+    int index = 0;
+    if (_num0s == 0) {return;}
+    for (int r = 0; r < 4; r++) {
+      for (int c = 0; c < 4; c++) {
+        if (index == pos) {
+          _board[r][c].set(((int)(Math.random()*2)+1)*2);
+          _num0s--;
+          return;
+        }
+        index++;
+      }
+    }
+  }
 }
