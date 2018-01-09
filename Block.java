5fe76a5466492a03d200348408d829f03958368e
Kevin Lin <33496321+Stuy-Kevin-Lin@users.noreply.github.com>
2018-01-08 19:34:11 -0500

    Indenting

--- Block.java
+++ Block.java
@@ -1,24 +1,24 @@
 public class Block{
-    private int _num;
-    public Block(){
-	_num=0;
+  private int _num;
+  public Block(){
+    _num=0;
+  }
+  public boolean isEmpty(){
+    return _num==0;
+  }
+  public int set(int newVal){
+    int oldVal=_num;
+    _num=newVal;
+    return oldVal;
+  }
+  public int get(){
+    return _num;
+  }
+  public String toString(){
+    String retStr=""+_num;
+    for(int i=0; i<4-retStr.length();i++){
+      retStr=" "+retStr;
     }
-    public boolean isEmpty(){
-	return _num==0;
-    }
-    public int set(int newVal){
-	int oldVal=_num;
-	_num=newVal;
-	return oldVal;
-    }
-    public int get(){
-	return _num;
-    }
-    public String toString(){
-	String retStr=""+_num;
-	for(int i=0; i<4-retStr.length();i++){
-	    retStr=" "+retStr;
-	}
-	return retStr;
-    }
+    return retStr;
+  }
 }
