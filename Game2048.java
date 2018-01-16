0a2e68d40800b3b12c23e7945f7bac9e94ef883f
Stuy-Kevin-Lin <klin14@stuy.edu>
2018-01-16 12:51:49 -0500

    Comments

--- Game2048.java
+++ Game2048.java
@@ -2,20 +2,20 @@ import cs1.Keyboard;
 public class Game2048{
     public static void main(String [] args){
 	Board b = new Board();
-	b.addBlock();
+	b.addBlock(); //Add two blocks for the start of a board
 	b.addBlock();
 	boolean badInput = false;
 	while (b.gameStatus().equals("Ongoing")){
-	    for (int i = 0; i < 64; i++) { System.out.println(); }
-	    System.out.println("Score: " + b.getScore());
-	    System.out.println(b);
-	    System.out.println("Pick a direction: Up, Left, Down, or Right.");
+	    for (int i = 0; i < 64; i++) { System.out.println(); } //"Clears" terminal
+	    System.out.println("Score: " + b.getScore()); //Prints the score of the board
+	    System.out.println(b); //Prints board
+	    System.out.println("Pick a direction: Up, Left, Down, or Right."); //Prints instructions
 	    System.out.println("Type in w, a, s, or d for each direction, respectively.");
-	    if (badInput) {System.out.println("Invalid move. Please try again.");}
-	    else {System.out.println();}
-	    badInput = false;
+	    if (badInput) {System.out.println("Invalid move. Please try again.");} //If a bad move was made last iteration, this message displays
+	    else {System.out.println();} //Else it's a blank line
+	    badInput = false; //Resets badInput
 	    String input = Keyboard.readString().toLowerCase();
-	    if (input.equals("w"))
+	    if (input.equals("w")) //Does action based on input
 		b.slideUp();
 	    else if (input.equals("s"))
 		b.slideDown();
@@ -24,14 +24,14 @@ public class Game2048{
 	    else if (input.equals("d"))
 		b.slideRight();
 	    else{
-		badInput = true;
+		badInput = true; //Sets badInput for next iteration
 	    }
 	}
-	if (b.gameStatus().equals("Loss")){
+	if (b.gameStatus().equals("Loss")){ //Returns loss message
 	    System.out.println(b);
 	    System.out.println("Sorry, you lost.");
 	}
-	else{
+	else{ //Returns win message
 	    System.out.println(b);
 	    System.out.println("Congrats! You won!");
 	}
