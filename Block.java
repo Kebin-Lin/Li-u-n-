542367a7161fa359cdc33f70919431a8597a1e77
Stuy-Kevin-Lin <klin14@stuy.edu>
2018-01-16 12:55:39 -0500

    Comments

--- Block.java
+++ Block.java
@@ -1,23 +1,23 @@
 public class Block{
-  private int _num;
-  public Block(){
-    _num=0;
-  }
-  public boolean isEmpty(){
-    return _num==0;
-  }
-  public int set(int newVal){
-    int oldVal=_num;
-    _num=newVal;
-    return oldVal;
-  }
-  public int get(){
-    return _num;
-  }
-  public String toString(){
-    if (_num == 0) {return "    ";}
-    String retStr=""+_num;
-    while (retStr.length() < 4) {retStr = " " + retStr;}
-    return retStr;
-  }
+    private int _num;
+    public Block(){
+	_num=0;
+    }
+    public boolean isEmpty(){
+	return _num==0;
+    }
+    public int set(int newVal){
+	int oldVal=_num;
+	_num=newVal;
+	return oldVal;
+    }
+    public int get(){
+	return _num;
+    }
+    public String toString(){
+	if (_num == 0) {return "    ";} //Returns a blank if _num is 0 ("Empty" block)
+	String retStr=""+_num; //Turns _num into a String
+	while (retStr.length() < 4) {retStr = " " + retStr;} //Makes retStr four characters long
+	return retStr;
+    }
 }
